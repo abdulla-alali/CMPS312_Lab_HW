39821d0c287586f87d1a96b05bc8fd62b427a198
Abdulahi Hassen <abdala.baaji@gmail.com>
2016-10-09 12:05:37 +0300

    Homework2: Add skeleton files

---
+++ HomeWork2/app/src/main/java/qa/edu/qu/cmps312/homework2/SecondActivity.java
@@ -0,0 +1,25 @@
+package qa.edu.qu.cmps312.homework2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+
+
+    public void closeActivity(View view) {
+
+        finish();
+    }
+
+
+
+}
